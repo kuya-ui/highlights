ec2c46be6cbdacf67a172fce1a29049be38442f7
kuya-ui <kuyamaxmillan@gmail.com>
2021-08-11 22:12:09 +0300

    initial

---
+++ start.sh
@@ -0,0 +1,4 @@
+export NEWS_API_KEY='8120360ba9e342dbaccb75e01109ca34'
+export SECRET_KEY='millan'
+
+python3.6 manage.py server 
---
+++ tests/articles_test.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append("../")
+import unittest
+from app.models import Articles
+
+class ArticlesTest(unittest.TestCase):
+    '''
+    Test Class to test the behavior of the articles class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_articles = Articles()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_articles,Articles))
+
+
+if __name__ == "__main__":
+    unittest.main()
